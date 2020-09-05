4710a181eb0f9f55effcff2dfc4d19eb3a852743
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2020-09-05 18:11:45 +0100

    feat: user registration

--- application/models.py
+++ application/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+def profile_image(instance, filename):
+    ext = filename.split('.')[-1]
+    return 'image/user_{0}.{1}'.format(instance.user.id, ext)
+
+class Member(models.Model):
+    GENDER = [
+        ('F', 'Female'),
+        ('M', 'Male')
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    DOB = models.DateField()
+    about = models.TextField()
+    image = models.ImageField(null=True, upload_to=profile_image)
+    gender = models.CharField(choices=GENDER, max_length=30)
+    address = models.TextField()
+    fav_color = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.user.username
--- application/views.py
+++ application/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+
+from application.models import Member
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        fav_color = request.POST['fav_color']
+        DOB = request.POST['DOB']
+        password = request.POST['password']
+        address = request.POST['address']
+        about = request.POST['about']
+        image = request.FILES['image']
+        gender = request.POST['gender']
+
+        user = User.objects.create_user(username=username, email=email,
+                                        password=password,
+                                        first_name=first_name,
+                                        last_name=last_name)
+        user.save()
+        member = Member.objects.create(user=user, fav_color=fav_color,
+                                       DOB=DOB, address=address, about=about,
+                                       image=image, gender=gender)
+        member.save()
+        return redirect('application:login')
+    elif request.method == 'GET':
+        return render(request, 'register.html')
+
+
+def login_view(request):
+    return None
+
+
+def logout_view(request):
+    return None
